7dbfe03a6f13676322df14ca2cab2a7427bd7958
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2021-10-07 00:28:14 +0200

    git ignored modified

--- TourGuide/src/test/java/tourGuide/service/TourGuideServiceTest.java
+++ TourGuide/src/test/java/tourGuide/service/TourGuideServiceTest.java
@@ -2,43 +2,56 @@ package tourGuide.service;
 
 
 import gpsUtil.GpsUtil;
+import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import rewardCentral.RewardCentral;
+import tourGuide.exception.UserAlreadyExistsException;
+import tourGuide.exception.UserNotFoundException;
+import tourGuide.helper.InternalTestHelper;
 import tourGuide.model.User;
-import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @SpringBootTest
-//@RunWith(SpringRunner.class)
-//@RunWith(MockitoJUnitRunner.class)
-//@ExtendWith(MockitoExtension.class)
-//@RunWith(JUnitPlatform.class)
 public class TourGuideServiceTest {
 
-    private Logger logger = LoggerFactory.getLogger(TourGuideServiceTest.class);
+    private final static Logger logger = LoggerFactory.getLogger(TourGuideServiceTest.class);
+    private GpsUtil gpsUtil = new GpsUtil();
+    private RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
+    private TourGuideService tourGuideService;
+    private User user;
 
-/*    @BeforeAll
-    public static void setUp(){
+    @BeforeAll
+    public static void setup(){
+
+        logger.debug("@BeforeAll");
         InternalTestHelper.setInternalUserNumber(0); //set the list of user to 0
         Locale.setDefault(new Locale("en", "US")); //Set default locale to avoid problems with comma between "," and "."
     }
 
     @BeforeEach
-    void setUpBeforeEach() throws UserAlreadyExistsException {
-        logger.debug("before each");
+    void init() throws UserAlreadyExistsException {
+        logger.debug("@BeforeEach");
         tourGuideService = new TourGuideService(gpsUtil,rewardsService);
         tourGuideService.tracker.stopTracking();
+
         user = new User(UUID.randomUUID(), "jon", "000", "jon@tourGuide.com");
         tourGuideService.addUser(user);
+
     }
 
     @AfterEach
-    void setUpAfterEach(){
-        logger.debug("tear down");
-        //tourGuideService.getInternalUserMap().remove(user.getUserName());
+    void tearDown(){
+        logger.debug("@AfterEach");
+        InternalTestHelper.freeInternalUserMap();
     }
 
     @Test
@@ -48,5 +61,12 @@ public class TourGuideServiceTest {
         Assertions.assertEquals("000", tourGuideService.getUser(user.getUserName()).getPhoneNumber());
         Assertions.assertEquals("jon@tourGuide.com", tourGuideService.getUser(user.getUserName()).getEmailAddress());
 
-    }*/
+    }
+
+    @Test
+    public void getUser_shouldThrowUserNotFoundException() throws UserNotFoundException {
+
+        assertThrows(UserNotFoundException.class, () -> tourGuideService.getUser("test"));
+
+    }
 }
