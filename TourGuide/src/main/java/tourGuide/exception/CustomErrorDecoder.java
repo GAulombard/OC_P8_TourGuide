3f0946c2600090e7f928ab0596051d9e6ea545ea
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2021-11-10 14:36:28 +0100

    added a custom error decoder for feign exceptino

---
+++ TourGuide/src/main/java/tourGuide/exception/CustomErrorDecoder.java
@@ -0,0 +1,28 @@
+package tourGuide.exception;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+
+
+public class CustomErrorDecoder implements ErrorDecoder {
+
+    private final ErrorDecoder defaultErrorDecoder = new Default();
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+
+        if(response.status() == 404) {
+            return new UserNotFoundException("User not found");
+        }
+
+        if(response.status() == 500) {
+            return new UsersGatheringException(" User gathering exception");
+        }
+
+        if(response.status() == 409) {
+            return  new UserAlreadyExistsException("User already exist");
+        }
+
+        return defaultErrorDecoder.decode(methodKey,response);
+    }
+}
--- gpsUtil/src/test/java/com/api/gpsutil/GpsUtilApplicationTests.java
+++ gpsUtil/src/test/java/com/api/gpsutil/GpsUtilApplicationTests.java
@@ -1,10 +1,25 @@
 package com.api.gpsutil;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Locale;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GpsUtilApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeAll
+    public static void setUp() {
+        Locale.setDefault(new Locale("en", "US"));
+    }
 
     @Test
     void contextLoads() {
